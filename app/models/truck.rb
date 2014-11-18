b7c5e7f48bbbdbdb81d672668fca992c17a601d5
Stephanie-Chou <stephanie.chou@outlook.com>
2014-11-17 22:34:38 -0800

    get trucks

--- app/models/truck.rb
+++ app/models/truck.rb
@@ -1,3 +1,12 @@
+require 'pry'
 class Truck < ActiveRecord::Base
   belongs_to :location
+
+  def self.get_trucks(locations)
+    trucks = []
+    locations.each do |location|
+      trucks<<Truck.where(location_id: location)
+    end
+    trucks.flatten
+  end
 end
