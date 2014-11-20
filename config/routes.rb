dfabcf2950a09aba6b5e94de2e2c664b63743296
Stephanie-Chou <stephanie.chou@outlook.com>
2014-11-19 16:13:26 -0800

    truck rest rouutes

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'welcome#index'
-
-  get 'nearestTrucks' => 'welcome#nearestTrucks'
+  resources :trucks
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
