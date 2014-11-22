d46cf0224b0523f511528965ca62fef43f6730d6
Stephanie-Chou <stephanie.chou@outlook.com>
2014-11-22 10:40:21 -0800

    updated truck model spec

--- spec/truck_spec.rb
+++ spec/truck_spec.rb
@@ -1,27 +1,37 @@
 require 'rails_helper'
-
+require 'pry'
 RSpec.describe Truck, :type => :model do
-  it "finds trucks with certain locations" do
-    Location.create!(latitude: 0, longitude: 0)
-    truck1 = Truck.create!(location_id: 1)
-    truck2 = Truck.create!(location_id: 2)
+  let!(:location) {Location.create!(latitude: 0, longitude: 0)}
+  let!(:truck1) {Truck.create!(fooditems: "meat: potatoes", location_id: 1)}
+  let!(:truck2) {Truck.create!(fooditems:"cookies: iced coffee", location_id: 2)}
+  let!(:food1) {Food.create!(name: 'meat')}
+  let!(:food2) {Food.create!(name: 'potatoes')}
+  let!(:food3) {Food.create!(name: 'cookies')}
+  let!(:food4) {Food.create!(name: 'iced coffee')}
 
+
+  it "finds trucks with certain locations" do
     expect(Truck.get_trucks([1,2])).to eq([truck1,truck2])
   end
 
   it "does not find trucks not in certain locations" do
-    Location.create!(latitude: 0, longitude: 0)
-    truck1 = Truck.create!(location_id: 1)
-    truck2 = Truck.create!(location_id: 2)
-
     expect(Truck.get_trucks([1])).to eq([truck1])
   end
 
   it "searches by fooditem" do
-    truck1 = Truck.create!(fooditems: "meat: potatoes")
-    truck2 = Truck.create!(fooditems:"cookies")
+      MenuItem.create!(truck: truck1, food: food1)
+      MenuItem.create!(truck: truck1, food: food2)
+      MenuItem.create!(truck: truck2, food: food3)
+      MenuItem.create!(truck: truck2, food_id: food4)
     trucks = Truck.all
 
-    expect(Truck.search_by_fooditem("cookies", trucks)).to eq([truck2])
+    expect(Truck.search_by_fooditem(["cookies"], trucks)).to eq([truck2])
+  end
+
+  it "searches by fooditem and finds partial matches" do
+    MenuItem.create!(truck: truck1, food: food1)
+    MenuItem.create!(truck: truck2, food: food4)
+
+    expect(Truck.search_by_fooditem(["coffees"], [truck2])).to eq([truck2])
   end
 end
