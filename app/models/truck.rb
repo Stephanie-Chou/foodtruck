60caefe5b943ffaf722773496aa7ed2ee09c5a3a
Stephanie-Chou <stephanie.chou@outlook.com>
2014-11-21 23:51:03 -0800

    filter matches partials

--- app/models/truck.rb
+++ app/models/truck.rb
@@ -13,6 +13,19 @@ class Truck < ActiveRecord::Base
   end
 
   def self.search_by_fooditem(terms, trucks)
-    trucks.select{|truck| (!(truck.foods.pluck('name') & terms).empty?)  unless truck.fooditems.nil?}
+
+    trucks.select{|truck| included?(terms, truck.foods.pluck('name'))  unless truck.fooditems.nil?}
   end
+
+  private
+
+  def self.included?(terms, foods)
+    terms.each do |term|
+      foods.each do |food|
+        return true if (food.include?(term) || food.include?(term[0...-1]))
+      end
+    end
+    return false
+  end
+
 end
